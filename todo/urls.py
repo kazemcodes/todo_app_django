cd502b66ee9715832148cf8030ffb50c6ed8f837
kazem <kazem.codes@gmail.com>
2024-06-15 22:32:19 +0330

    # 13 add listview

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('tasks/', views.TaskList.as_view(template_name='task_list.html'), name='tasks')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
 
+from todo.models import Task
 
 
 
 def home(request):
     return render(request,'home.html')    
+
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
