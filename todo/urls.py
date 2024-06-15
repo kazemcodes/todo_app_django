419ac807399b7271a693dbf86aab3bc95402f5b0
kazem <kazem.codes@gmail.com>
2024-06-15 23:34:44 +0330

    #15 update view

--- todo/urls.py
+++ todo/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('tasks/', views.TaskList.as_view(template_name='todo/task_list.html'), name='tasks'),
-    path('task/<int:pk>/', views.TaskDetail.as_view(template_name='todo/task_detail.html'), name='task')
+    path('task/<int:pk>/', views.TaskDetail.as_view(template_name='todo/task_detail.html'), name='task'),
+   
+    path('task/update/<int:pk>/', views.TaskUpdate.as_view(template_name='todo/task_form.html'),name='task-update'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-
+from django.views.generic.edit import UpdateView, CreateView, DeleteView
+from django.urls import reverse_lazy
 from todo.models import Task
+from django.contrib import messages
 
 
 
@@ -17,3 +19,12 @@ class TaskList(ListView):
 class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
+    
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = ['title','description','completed']
+    success_url = reverse_lazy('tasks')
+    
+    def form_valid(self,form):
+        messages.success(self.request, "Task updated successfully")
+        return super().form_valid(form)
