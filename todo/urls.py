620505f8f9870b5a4f7e9fb98470311b2c2b569e
kazem <kazem.codes@gmail.com>
2024-06-16 00:11:20 +0330

    #17 register and permission restriction

--- todo/urls.py
+++ todo/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('task/<int:pk>/', views.TaskDetail.as_view(template_name='todo/task_detail.html'), name='task'),
     path('task/create/', views.TaskCreate.as_view(template_name='todo/task_form.html'), name='task-create'),
     path('task/update/<int:pk>/', views.TaskUpdate.as_view(template_name='todo/task_form.html'),name='task-update'),
+    path('task/delete/<int:pk>/', views.TaskDelete.as_view(template_name='todo/delete_task.html'),name='task-delete'),
+    path('register/', views.RegisterUser.as_view(template_name='todo/register.html'), name='register'),
+    path('logout/', views.RegisterUser.as_view(template_name='todo/register.html'), name='logout'),
+    path('login/', views.RegisterUser.as_view(template_name='todo/register.html'), name='login'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import UpdateView, CreateView, DeleteView
+from django.views.generic.edit import UpdateView, CreateView, FormView,DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from django.urls import reverse_lazy
 from todo.models import Task
 from django.contrib import messages
@@ -16,9 +20,18 @@ class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        return context
+    
 class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
+    
+    def get_queryset(self):
+        base_qs = super(TaskDetail, self).get_queryset()
+        return base_qs.filter(user=self.request.user)  
     
 class TaskUpdate(UpdateView):
     model = Task
@@ -29,6 +42,9 @@ class TaskUpdate(UpdateView):
         messages.success(self.request, "Task updated successfully")
         return super().form_valid(form)
     
+    def get_queryset(self):
+        base_qs = super(TaskUpdate, self).get_queryset()
+        return base_qs.filter(user=self.request.user)
 class TaskCreate(CreateView):
     model = Task
     fields = ['title','description','completed']
@@ -38,3 +54,29 @@ class TaskCreate(CreateView):
         form.instance.user = self.request.user
         messages.success(self.request, "Task Create successfully")
         return super().form_valid(form)
+
+class TaskDelete(LoginRequiredMixin, DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
+    
+    def form_valid(self, form):
+        messages.success(self.request, "The task was deleted successfully.")
+        return super(TaskDelete,self).form_valid(form)
+      
+    def get_queryset(self):
+        base_qs = super(TaskDelete, self).get_queryset()
+        return base_qs.filter(user=self.request.user)
+    
+class RegisterUser(FormView):
+    template_name = 'register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('tasks')
+    redirect_authenticated_user = True
+    
+    def form_valid(self,form):
+        user = form.save()
+        if user:
+            login(self.request, user)
+        return super().form_valid(form)
+    
